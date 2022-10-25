2beac38dbab54a0650c89416b5f549b626212687
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-10-25 22:13:50 +0500

    working on application inital functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  const [values,SetValues]=useState('')
-  const [error, Seterror] = useState(true);
-  return <h2>color generator project</h2>
+  const [color, Setcolor] = useState("#f15025");
+  const [error, Seterror] = useState(false);
+  const [list, Setlist] = useState([]);
+  console.log(list);
+  const handleChange = (e) => {
+    let colors = e.target.value;
+    Setcolor(colors);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    try {
+      let colors= new Values(color).all(10)
+      Setlist(colors);
+    } catch (error) {
+      Seterror(true)
+    }
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={submitHandler}>
+          <input type="text" value={color} onChange={handleChange} />
+          <button type="submit" className="btn">
+            submit
+          </button>
+        </form>
+      </section>
+      <SingleColor />
+    </>
+  );
 }
 
-export default App
+export default App;
