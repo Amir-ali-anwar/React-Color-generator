3501b44c20abc32ff5b8bf8bd1046c7085fa2d97
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-10-25 20:49:51 +0500

    inital project setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
+  const [values,SetValues]=useState('')
+  const [error, Seterror] = useState(true);
   return <h2>color generator project</h2>
 }
 
